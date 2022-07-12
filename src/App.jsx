aecbbee648070046724c922880949dde0e8cdad9
Ajmal Nasumudeen <ajmal.n@trois.in>
2022-07-12 14:18:31 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import FRParent from './components/FRParent'
-
 function App() {
+  console.log('test', process.env)
   return (
     <div style={{ display: "flex", justifyContent: "center", flexDirection: 'column', alignItems: 'center' }} >
       <p> App created by ajmal</p>
@@ -9,7 +9,7 @@ function App() {
       <p>changes </p>
       <FRParent />
       <div style={{ backgroundColor: 'black', width: '100%' }}>
-        <p style={{ color: 'white', alignContent: 'center', fontSize: '700', textAlign: 'center' }}>  version:  {process.env.npm_package_version} </p>
+        <p style={{ color: 'white', alignContent: 'center', fontSize: '700', textAlign: 'center' }}>  version:  {process.env.REACT_APP_VERSION} </p>
       </div>
 
     </div>
