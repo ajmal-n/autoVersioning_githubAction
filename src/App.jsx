b872a0fde8e7952d4465713597be122414f357f1
Ajmal Nasumudeen <ajmal.n@trois.in>
2022-07-12 16:55:12 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   console.log('test', process.env)
   return (
     <div style={{ display: "flex", justifyContent: "center", flexDirection: 'column', alignItems: 'center' }} >
-
+      <p>Changes 1</p>
       <FRParent />
       <div style={{ backgroundColor: 'black', width: '100%' }}>
         <p style={{ color: 'white', alignContent: 'center', fontSize: '700', textAlign: 'center' }}>  version:  {process.env.REACT_APP_VERSION} </p>
