800b021a59fb4af89e596ae9392e19b338b017a6
Ajmal Nasumudeen <ajmal.n@trois.in>
2022-07-12 13:33:21 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div style={{ display: "flex", justifyContent: "center", flexDirection: 'column', alignItems: 'center' }} >
       <p> App created by ajmal</p>
       <p>some changes in production</p>
-      <p>Chnages </p>
+      <p>changes </p>
       <FRParent />
       <div style={{ backgroundColor: 'black', width: '100%' }}>
         <p style={{ color: 'white', alignContent: 'center', fontSize: '700', textAlign: 'center' }}>  version:  {version} </p>
